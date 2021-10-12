d812cad076a22847093d5bccbaa5d79aaf91b599
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-10-12 19:18:39 +0300

    feat: Remove shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.redmadrobot.build.dsl.*
+
 plugins {
     id("redmadrobot.root-project") version "0.12.1"
 }
@@ -5,5 +7,15 @@ plugins {
 redmadrobot {
     publishing {
         signArtifacts.set(true)
+
+        pom {
+            setGitHubProject("osipxd/encrypted-datastore")
+            licenses {
+                mit()
+            }
+            developers {
+                developer(id = "osipxd", name = "Osip Fatkullin", email = "osip.fatkullin@gmail.com")
+            }
+        }
     }
 }
--- encrypted-datastore/build.gradle.kts
+++ encrypted-datastore/build.gradle.kts
@@ -1,37 +1,30 @@
-import com.redmadrobot.build.dsl.*
-
 plugins {
     id("redmadrobot.kotlin-library")
     id("redmadrobot.publish")
-    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 description = "Extensions to encrypt DataStore"
 group = "io.github.osipxd"
 version = "1.0.0-alpha01"
 
+val hackProject = project(":encrypted-datastore-internal-visibility-hack")
 dependencies {
     api(kotlin("stdlib"))
     api("androidx.datastore:datastore-core:1.0.0")
     api("androidx.datastore:datastore-preferences-core:1.0.0")
     api("com.google.crypto.tink:tink-android:1.6.1")
 
-    // It will be embedded into shadowJar and shouldn't be added to pom.xml file
-    compileOnly(project(":encrypted-datastore-internal-visibility-hack"))
+    // It will be embedded into jar and shouldn't be added to pom.xml file
+    compileOnly(hackProject)
 }
 
 repositories {
     google()
 }
 
-// We use shadowJar instead
-tasks.jar { enabled = false }
-
-tasks.shadowJar {
-    configurations = listOf(project.configurations.compileOnly.get())
-    dependencies {
-        include(project(":encrypted-datastore-internal-visibility-hack"))
-    }
+// Embed hack into the jar
+tasks.jar {
+    from(hackProject.sourceSets["main"].output)
 }
 
 publishing {
@@ -39,25 +32,6 @@ publishing {
         maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
             name = "ossrh"
             credentials(PasswordCredentials::class)
-        }
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications.getByName<MavenPublication>("maven") {
-            // Exclude plain jar, leave shadowJar only
-            setArtifacts(artifacts.filterNot { it.extension == "jar" && it.classifier == null })
-
-            pom {
-                setGitHubProject("osipxd/encrypted-datastore")
-                licenses {
-                    mit()
-                }
-                developers {
-                    developer(id = "osipxd", name = "Osip Fatkullin", email = "osip.fatkullin@gmail.com")
-                }
-            }
         }
     }
 }
