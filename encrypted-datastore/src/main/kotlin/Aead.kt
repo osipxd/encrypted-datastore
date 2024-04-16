4230eff29887499324bde2d1761f00aa08ceca22
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-04-16 11:33:04 +0200

    docs: Small docs fixes

--- encrypted-datastore/src/main/kotlin/Aead.kt
+++ encrypted-datastore/src/main/kotlin/Aead.kt
@@ -5,7 +5,7 @@ import java.io.InputStream
 
 internal fun Aead.newDecryptedStream(inputStream: InputStream): InputStream {
     // Method 'decrypt' throws GeneralSecurityException for empty byte array,
-    // so let's check it is not empty.
+    // so check it is not empty.
     return if (inputStream.available() > 0) {
         decrypt(inputStream.readBytes(), null).inputStream()
     } else {
