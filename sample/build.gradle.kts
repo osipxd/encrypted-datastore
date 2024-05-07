ae1257fa828aa378b7459f0b9fdd2e56faddf7a0
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-05-07 10:21:42 +0200

    sample: Init sample from template

---
+++ sample/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    com.android.application
+    org.jetbrains.kotlin.android
+}
+
+android {
+    namespace = "com.example.sample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.sample"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.core)
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+
+    implementation(projects.securityCryptoDatastore)
+    implementation(projects.securityCryptoDatastorePreferences)
+}
