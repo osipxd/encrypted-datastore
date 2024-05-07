5492aa944693ec210007187ceb13e44710ad0bbf
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-05-08 00:16:10 +0200

    fix: Do not publish test fixtures
    Closes: #32

--- buildSrc/src/main/kotlin/convention.publish.gradle.kts
+++ buildSrc/src/main/kotlin/convention.publish.gradle.kts
@@ -1,4 +1,7 @@
 import com.redmadrobot.build.dsl.ossrh
+import org.gradle.api.plugins.internal.JavaPluginHelper
+import org.gradle.internal.component.external.model.TestFixturesSupport
+import org.gradle.jvm.component.internal.DefaultJvmSoftwareComponent
 
 plugins {
     id("com.redmadrobot.publish")
@@ -8,4 +11,12 @@ publishing {
     repositories {
         ossrh { credentials(PasswordCredentials::class) }
     }
+}
+
+// Exclude test fixtures from publication, as we use it only internally
+plugins.withId("org.gradle.java-test-fixtures") {
+    val component = JavaPluginHelper.getJavaComponent(project) as DefaultJvmSoftwareComponent
+    val feature = component.features.getByName(TestFixturesSupport.TEST_FIXTURES_FEATURE_NAME)
+    component.withVariantsFromConfiguration(feature.apiElementsConfiguration) { skip() }
+    component.withVariantsFromConfiguration(feature.runtimeElementsConfiguration) { skip() }
 }
