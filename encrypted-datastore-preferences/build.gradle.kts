20613e431b025477375835d2093c2b34188942ee
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-05-07 10:16:36 +0200

    build: Enable typesafe project accessors

--- encrypted-datastore-preferences/build.gradle.kts
+++ encrypted-datastore-preferences/build.gradle.kts
@@ -5,13 +5,13 @@ plugins {
 description = "Extensions to encrypt DataStore Preferences using Tink"
 
 dependencies {
-    api(project(":encrypted-datastore"))
+    api(projects.encryptedDatastore)
     api(libs.androidx.datastore.preferences.core)
 
     testImplementation(kotlin("test", version = libs.versions.kotlin.get()))
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
-    testImplementation(testFixtures(project(":encrypted-datastore")))
+    testImplementation(testFixtures(projects.encryptedDatastore))
 }
 
 // All Java classes here are package-private, so don't generate javadoc
