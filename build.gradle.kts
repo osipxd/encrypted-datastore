2664485c1f447c6652441ed57edea8f25cc224b5
Dayan Ruben <mail@dayanruben.com>
2025-03-26 15:15:02 -0300

    Update versioning logic to include encrypted datastore version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 
 val datastoreVersion = libs.versions.androidx.datastore.get()
+val libVersion = libs.versions.encrypted.datastore.get()
 allprojects {
     group = "com.dayanruben"
-    version = "$datastoreVersion-beta05"
+    version = "$datastoreVersion-$libVersion"
 }
